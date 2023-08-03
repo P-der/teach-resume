dba3f77f739894bb029f03b7db46c062e4b69eb0
P-der <1198811801@qq.com>
2023-08-03 18:36:26 +0800

    init

---
+++ meta.config.js
@@ -0,0 +1,50 @@
+export default {
+  user: {
+    name: "庞得恩",
+    project: "前端开发工程师",
+    experience: "3年工作经验",
+    wechat: 18846935882,
+    phone: 18846935882,
+    email: "p_der1198811801@163.com",
+    address: "Beijing",
+    github: "https://github.com/P-der"
+  },
+  evaluation: "这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价",
+  workExperiences: [
+    {
+      name: 'ABC 公司',
+      time: {
+        start: '2021.8',
+        end: '2022.9'
+      },
+      projects: [
+        {
+          name: 'DEF 项目',
+          desc: '基于 star 面试法来写 - 背景 - 任务 - 行动 - 结果'
+        },
+        {
+          name: 'xxxx 项目',
+          desc: '基于 star 面试法来写 - 背景 - 任务 - 行动 - 结果'
+        }
+      ]
+    },
+    {
+      name: 'ABC 公司',
+      time: {
+        start: '2021.8',
+        end: '2022.9'
+      },
+      projects: [
+        {
+          name: 'DEF 项目',
+          desc: '基于 star 面试法来写 - 背景 - 任务 - 行动 - 结果'
+        },
+        {
+          name: 'xxxx 项目',
+          desc: '基于 star 面试法来写 - 背景 - 任务 - 行动 - 结果'
+        }
+      ]
+    }
+  ],
+  skills: ["Vue3", "js", "ts", "react"],
+};
